6ec03513b3b185e46bb2516966ab5f4d91d82ba2
Cristian Robles <cristianrobles2020@itp.edu.co>
2022-03-11 02:44:38 -0500

    Taller de UPDATE y DELETE en BD

--- Consulta Punto 3.sql
+++
@@ -1,5 +0,0 @@
-SELECT sucursal_cliente.`id_contacto` AS ID_CLIENTE, contactos.`nom_representante` AS NOMBRE_CLIENTE,  COUNT(sucursal_cliente.`id_sucursal`) AS CANTIDAD_SUCURSALES
-FROM sucursal_cliente
-INNER JOIN contactos ON sucursal_cliente.`id_contacto` = contactos.`id`
-GROUP BY sucursal_cliente.`id_contacto`
-
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#from crypt import methods
+#from crypt import methods
 from sqlite3 import Cursor
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, flash, render_template, request, redirect, url_for
 import mysql.connector #Conector de base de datos
 #render_template => Siempre lee la carpeta templates
 
@@ -50,8 +52,49 @@ def crearProducto():
     cursor.close()
     #Volver al listado o formulario
 
-
     return redirect(url_for('inicio'))
+
+    #Volver al listado o formulario
+
+    #return redirect(url_for('inicio'))
+
+#Eliminar Registros 
+@app.route("/eliminar/<int:id>")
+def formEliminarProducto(id):
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM productos WHERE id = {0}".format(id))
+    cursor.close()
+    return redirect(url_for('inicio'))
+
+#Editar productos
+@app.get("/form_editar")
+def formEditarProducto():
+    return render_template('editarProducto.html')
+
+@app.route('/editarProducto/<int:id>', methods = ['POST', 'GET'])
+def obtenerProducto(id):
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM productos WHERE id = {0}".format(id))
+    producto = cursor.fetchall()
+    cursor.close()
+    print (producto)
+    return render_template('editarProducto.html', producto = producto[0])
+#---------------------------------------------
+#Acctualiza productos
+@app.route('/actualizarProducto/<id>', methods=['POST'])
+def actualizaProductoId(id):
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        price = request.form['price']
+        cursor = db.cursor()
+        cursor.execute("""
+            UPDATE productos SET  nombre = %s, price = %s
+            WHERE id = %s
+        """, (nombre, price, id)) 
+        return redirect(url_for('inicio'))
+
+
+#------------------------------------------
 
 @app.get("/contactos")#Crear una ruta
 def listarContactos():
